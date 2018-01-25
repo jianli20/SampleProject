24862136cea8e2e41ec4038ce4923666e78a1511
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-25 11:52:17 -0700

    Added controller folder, added controller cpp and hpp , added runner cpp

---
+++ SampleProject/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  SampleProject
+//
+//  Created by Li, Jian Hong on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ SampleProject/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  SampleProject
+//
+//  Created by Li, Jian Hong on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
